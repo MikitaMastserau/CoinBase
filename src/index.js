9960aec32b942dc460ccd78f999b8315e0c18282
MikitaMastserau <nik.redko@gmail.com>
2023-08-16 18:16:30 +0700

    add main and reviews sections

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { MainLayout } from "components/MainLayout";
+import { HomeContainer } from "pages/Home/containers/HomeContainer";
 
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <MainLayout />
+  <MainLayout>
+    <HomeContainer />
+  </MainLayout>
 );
---
+++ src/pages/Home/components/HomeLayout/index.js
@@ -0,0 +1,11 @@
+import { Main } from "../Main";
+import { Reviews } from "../Reviews";
+
+export const HomeLayout = () => {
+   return (
+      <>
+         <Main />
+         <Reviews />
+      </>
+   )
+};
